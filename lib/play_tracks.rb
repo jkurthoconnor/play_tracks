bec8068f2de06fb677832098d30d810bdb71fcee
jkurthoconnor <jkurthoconnor@gmail.com>
2019-01-23 14:50:57 -0700

    Add application class to gem

--- lib/play_tracks.rb
+++ lib/play_tracks.rb
@@ -1,6 +1,14 @@
 require "play_tracks/version"
 
 module PlayTracks
+  class Application
+    def call(env)
+      [200,
+       {'Content-Type' => 'text/html'},
+       ["Hello from Ruby on Play Tracks."]]
+    end
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
